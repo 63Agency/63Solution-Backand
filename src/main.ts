c013aa8f98072a1569be4892722efb029df1f605
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2026-05-12 11:53:44 +0100

    Enhance CORS configuration in main.ts to specify allowed origins, methods, and headers. Update default port to 3002 for application startup.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+    origin: ['https://app.63agency.com', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -24,7 +29,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3002;
   await app.listen(port);
   logger.log(`Application démarrée sur le port ${port}`);
 }
