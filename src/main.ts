90af435ed7a01e3a01d15c63a34a49555a2d9d84
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2026-07-15 21:53:00 +0100

    Refactor CORS configuration in main.ts: Remove allowedHeaders for improved security and streamline CORS settings.

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ async function bootstrap() {
   app.enableCors({
     origin: ['https://app.63agency.com', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Signature'],
     credentials: true,
   });
   app.useGlobalPipes(
